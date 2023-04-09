86194099f0c3f9b46510142eecd061b2c6bb6338
Mukesh Gurpude <mukesh.intern@phonepe.com>
2023-04-09 10:55:29 +0530

    add logger

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import healthcheckRoutes from './routes/healthcheck'
 import { SERVER_PORT } from './routes/env'
+import logger from './routes/logger'
 
 const app = express()
 
 healthcheckRoutes(app)
 
 app.listen(SERVER_PORT, () => {
-    console.debug(`Server running on ${SERVER_PORT}`)
+    logger.debug(`Server running on ${SERVER_PORT}`)
 })
