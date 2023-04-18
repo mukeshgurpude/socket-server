2ea5c6a1171d096fdc4f19e23faa0a470597b064
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2023-04-18 13:59:48 +0530

    admin: enable admin-ui based on configuration file

--- routes/socket.ts
+++ routes/socket.ts
@@ -1,9 +1,25 @@
 import type { Server } from 'http'
-import { Server as WebSocketServer } from 'socket.io'
+import { ServerOptions, Server as WebSocketServer } from 'socket.io'
 import logger from '../utils/logger'
+import config from '../utils/config'
+import { instrument } from '@socket.io/admin-ui'
 
 export default function connectSocket(server: Server) {
-    const io = new WebSocketServer(server)
+    // TODO: move to some constants file
+    const wsConfig: Partial<ServerOptions> = config.enableAdmin ? {
+        cors: {
+            origin: ['https://admin.socket.io'],
+            credentials: true
+        }
+    } : {}
+
+    const io = new WebSocketServer(server, wsConfig)
+
+    if (config.enableAdmin) {
+        instrument(io, {
+            auth: false
+        })
+    }
 
     io.on('connection', (socket) => {
         logger.debug(`A user connected: ${socket.id}`)
@@ -11,6 +27,8 @@ export default function connectSocket(server: Server) {
         socket.on('disconnect', () => {
             logger.debug(`${socket.id} disconnected`)
         })
+
+        // TODO: add events as per configuration
     })
 
     return io
